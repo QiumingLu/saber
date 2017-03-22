6a9a77a88651c70f8857b533babe482e7fdc6cec
mirantslu(卢秋明) <mirantslu@tencent.com>
2017-03-22 09:32:33 +0800

    fix macOS build

--- cmake/Targets.cmake
+++ cmake/Targets.cmake
@@ -9,7 +9,11 @@ add_library(proto STATIC ${proto_hdrs} ${proto_srcs})
 target_link_libraries(proto PUBLIC ${PROTOBUF_LIBRARIES})
 target_include_directories(proto PUBLIC ${PROTOBUF_INCLUDE_DIR})
 
+install(FILES ${proto_hdrs} DESTINATION include/saber/proto)
+
+
 list(INSERT Saber_LINKER_LIBS 0 PUBLIC proto)
+
 
 file(GLOB_RECURSE saber_server_srcs
   src/saber/util/*.cc
@@ -27,30 +31,5 @@ file(GLOB_RECURSE saber_client_srcs
   src/saber/storage/.*cc
   src/saber/service/*.cc
   src/saber/client/*.cc
-  $${proto_srcs}
+  ${proto_srcs}
 )
-
-add_executable(saber_server src/saber/server/saber_server_main.cc ${saber_server_srcs})
-add_executable(saber_client src/saber/client/saber_client_main.cc ${saber_client_srcs})
-
-target_link_libraries(saber_server ${Saber_LINKER_LIBS})
-target_link_libraries(saber_client ${Saber_LINKER_LIBS})
-
-if(CMAKE_BUILD_SHARED_LIBS)
-  add_library(saber SHARED ${saber_client_srcs})
-  add_library(saber_static STATIC ${saber_client_srcs})
-  set_target_properties(saber_static PROPERTIES OUTPUT_NAME "saber")
-  set_target_properties(saber PROPERTIES CLEAN_DIRECT_OUTPUT 1)
-  set_target_properties(saber_static PROPERTIES CLEAN_DIRECT_OUTPUT 1)
-  set_target_properties(saber PROPERTIES VERSION 1.0 SOVERSION 1)
-  target_link_libraries(saber ${Saber_LINKER_LIBS})
-  install(TARGETS saber DESTINATION lib)
-  install(TARGETS saber_static LIBRARY DESTINATION lib ARCHIVE DESTINATION lib)
-else()
-  add_library(saber ${saber_client_srcs})
-  target_link_libraries(saber ${Saber_LINKER_LIBS})
-  install(TARGETS saber DESTINATION lib)
-endif()
-
-install(DIRECTORY ${Saber_INCLUDE_DIRS}/saber DESTINATION include)
-install(FILES ${proto_hdrs} DESTINATION include/saber/proto)
